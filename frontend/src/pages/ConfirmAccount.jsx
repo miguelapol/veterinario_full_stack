71bd66807940a51fe398956eb0eaa8e71e0ca340
miguelapol <apolonio1995@gmail.com>
2023-07-31 20:45:20 -0600

    Agregando archivos frontend correspondientes

---
+++ frontend/src/pages/ConfirmAccount.jsx
@@ -0,0 +1,58 @@
+import {useEffect,useState} from 'react'
+import {useParams,Link} from 'react-router-dom'
+import Alerta from '../components/Alerta';
+import ClienteAxios from '../config/axios'
+function ConfirmAccount() {
+//Alerta
+
+const [cuentaConfirmada,setCuentaConfirmada] =useState(false)
+const [cargando,setCargando]=useState(true)
+const [alerta,setAlerta]=useState({})
+
+const params= useParams()
+//con este extraemos los parametros
+const {id}=params
+// usamos el arreglo para que solo se ejecute una sola vez
+useEffect(()=>{
+  const confirmarCuenta=async()=>{
+        try {
+          const url =`/veterinarios/confirmar/${id}`
+          const {data}=await ClienteAxios(url)
+          setCuentaConfirmada(true)
+          setAlerta({
+            msg:data.msg
+          })
+        } catch (error) {
+          setAlerta({
+            msg:error.response.data.msg,
+            error:true
+          })
+        }
+        setCargando(false)
+      }
+     confirmarCuenta()
+},[])
+    return (
+       <>
+         <div>
+          <h1 className="text-indigo-600 font-black text-6xl">
+            Confirma tu cuenta y empieza a administrar tus{""}
+            <span className="text-black">pacientes</span></h1>
+        </div>
+        <div className='mt-20 md:met-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
+          {!cargando && <Alerta
+                 alerta={alerta}
+                 />
+          }
+          {cuentaConfirmada&&(
+               <Link 
+               className='block text-center my-5 text-gray-500'
+               to="/">Iniciar sesion</Link>
+          )}
+          </div>
+       
+       </>
+     )
+   }
+   
+   export default ConfirmAccount
---
+++ frontend/src/pages/NuevoPassword.jsx
@@ -0,0 +1,103 @@
+import { useState,useEffect} from 'react'
+import { useParams,Link } from 'react-router-dom'
+import Alerta from '../components/Alerta'
+import clienteAxios from '../config/axios'
+function NuevoPassword() {
+  const [password,setPassword]=useState('')
+  const [alerta,setAlerta]=useState({})
+  const [tokenValido,setTokenValido]=useState(false)
+  const [passwordModificado,setPasswordModificado]=useState(false)
+  const params = useParams()
+  console.log(params)
+  const {token} = params
+  useEffect(()=>{
+      const comprobarToken= async()=>{
+        try {
+          await clienteAxios(`/veterinarios/olvide-password/${token}`)
+          setAlerta({
+            msg:'Coloca tu nuevo password'
+          })
+          setTokenValido(true)
+        } catch (error) {
+          setAlerta({
+            msg:'Hubo un error con el enlace',
+            error:true
+          })
+        }
+      }
+      comprobarToken()
+  },{})
+  const handleSubmit= async(e)=>{
+    e.preventDefault()
+    if(password.length<6){
+      setAlerta({
+        msg: 'El password debe ser minimo de 6',
+        error:true
+      })
+      return
+    }
+    try {
+      const url=`/veterinarios/olvide-password/${token}`
+      const {data} = await clienteAxios.post(url,{password})
+      setPasswordModificado(true)
+      setAlerta({
+        msg: data.msg
+      })
+    } catch (error) {
+      setAlerta({
+        msg:error.response.data.msg,
+        error:true
+      })
+    }
+  }
+  const {msg}=alerta
+  return (
+    <>
+      <div>
+          <h1 className="text-indigo-600 font-black text-6xl">
+            Restablece tu password y no pierdas el acceso a {""}
+            <span className="text-black">pacientes</span></h1>
+      </div>
+      <div className='mt-20 md:met-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
+      {msg&&<Alerta
+               alerta={alerta}
+      />}
+      { tokenValido &&(
+          <form onSubmit={handleSubmit}>
+                <div className="my-5">
+                        <label
+                          className="uppercase text-gray-600 block text-xl font-bold"
+                        >
+                        Nuevo password
+                        </label>
+                        <input type="password" 
+                        placeholder="Tu Nuevo password"
+                        className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+                        value={password}
+                        onChange={e=>{setPassword(e.target.value)}}/>
+
+                </div>
+                    <input 
+                type="submit"
+                value="Guardar nuevo password"
+                 className="bg-indigo-700 w-full py-3 px-10
+                 rounded-xl text-white uppercase font-bold
+                 mt-5 hover:cursor-pointer
+                 hover:bg-indigo-800
+                 md:w-auto 
+                 "
+                />
+          </form>
+
+      )}
+      {passwordModificado&&
+      <Link 
+      className='block text-center my-5 text-gray-500'
+      to="/">Inicia sesion</Link>
+      }
+      </div>
+    </>
+  )
+}
+
+export default NuevoPassword
